5a091b8d0b10faeea085e26af770cf209033b255
Mishra Ram Swarup <ram.mishra@nagra.com>
2019-12-05 18:50:39 +0530

    Updates

---
+++ precache-manifest.faea960a0670573d3e83a47a95a74cfb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "161d0e2d0305ce73a6ce4d2316d7f0d5",
+    "url": "/its-free/index.html"
+  },
+  {
+    "revision": "4cb1279c71c5bfe6a68e",
+    "url": "/its-free/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "030c9297c74fef0a1d37",
+    "url": "/its-free/static/js/2.56460b44.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/its-free/static/js/2.56460b44.chunk.js.LICENSE"
+  },
+  {
+    "revision": "4cb1279c71c5bfe6a68e",
+    "url": "/its-free/static/js/main.bfab3add.chunk.js"
+  },
+  {
+    "revision": "0924804c30ce7015d38c",
+    "url": "/its-free/static/js/runtime-main.2db41b29.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/its-free/static/media/logo.5d5d9eef.svg"
+  }
+]);
